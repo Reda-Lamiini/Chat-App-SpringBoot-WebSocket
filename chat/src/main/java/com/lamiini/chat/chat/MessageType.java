d21442302c26f561d245c99b09c57c1318922e7a
reda lamiini <lamiini2004@gmail.com>
2023-12-04 15:29:47 +0100

    code Source

---
+++ chat/src/main/java/com/lamiini/chat/chat/MessageType.java
@@ -0,0 +1,8 @@
+package com.lamiini.chat.chat;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVER,
+
+}
---
+++ chat/src/main/java/com/lamiini/chat/config/WebSocketEventListenner.java
@@ -0,0 +1,23 @@
+package com.lamiini.chat.config;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+
+public class WebSocketEventListenner {
+
+    @EventListener
+    public void HandleWebSocketDisconnectListenner (
+        SessionDisconnectEvent event
+    ){
+        // TODO -- To be implemented
+    }
+
+}
